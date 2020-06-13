39d9ac20535ab50ef68602c13c221703fa336c01
antonmalykhin <a.y.malykhin@gmail.com>
2020-06-13 17:39:05 +0300

    Добавляет snapshot тесты

---
+++ src/components/welcome-screen/welcome-screen.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import WelcomeScreen from './welcome-screen.jsx';
+
+const Settings = {
+  ERROR_COUNT: 3
+};
+
+it(`Should WelcomeScreen render correctly`, () => {
+  const tree = renderer
+    .create(<WelcomeScreen errorCount={Settings.ERROR_COUNT} />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
