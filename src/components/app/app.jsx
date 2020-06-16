7299b281d01dede1cf6788c9c3955c1c7299a23f
antonmalykhin <a.y.malykhin@gmail.com>
2020-06-16 18:33:38 +0300

    Настраивает роутинг

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
+import ArtistQuestionScreen from '../question-artist/question-artist.jsx';
+import GenreQuestionScreen from '../question-genre/question-genre.jsx';
 
 const welcomeButtonHandler = () => {};
 
@@ -8,10 +11,22 @@ const App = (props) => {
   const {errorCount} = props;
 
   return (
-    <WelcomeScreen
-      errorCount={errorCount}
-      onWelcomeButtonClick={welcomeButtonHandler}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen
+            errorCount={errorCount}
+            onWelcomeButtonClick={welcomeButtonHandler}
+          />
+        </Route>
+        <Route exact path="/dev-artist">
+          <ArtistQuestionScreen />
+        </Route>
+        <Route exact path="/dev-genre">
+          <GenreQuestionScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
