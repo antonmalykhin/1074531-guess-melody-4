110f795695f5b8804c755fe33d97d4b74bf27ef3
antonmalykhin <a.y.malykhin@gmail.com>
2020-06-17 14:06:09 +0300

    Обновляет тест

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -2,13 +2,54 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import App from './app.jsx';
 
+const AVATAR_URL = `https://api.adorable.io/avatars/128`;
+
 const Settings = {
   ERROR_COUNT: 3
 };
 
+const questions = [
+  {
+    type: `genre`,
+    genre: `rock`,
+    answers: [{
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      genre: `rock`,
+    }, {
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      genre: `blues`,
+    }, {
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      genre: `jazz`,
+    }, {
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      genre: `rock`,
+    }],
+  }, {
+    type: `artist`,
+    song: {
+      artist: `Jim Beam`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+    },
+    answers: [{
+      picture: `${AVATAR_URL}/${Math.random()}`,
+      artist: `John Snow`,
+    }, {
+      picture: `${AVATAR_URL}/${Math.random()}`,
+      artist: `Jack Daniels`,
+    }, {
+      picture: `${AVATAR_URL}/${Math.random()}`,
+      artist: `Jim Beam`,
+    }],
+  }
+];
+
 it(`Render App`, () => {
   const tree = renderer
-    .create(<App errorCount={Settings.ERROR_COUNT} />)
+    .create(<App
+      errorCount={Settings.ERROR_COUNT}
+      questions={questions}
+    />)
     .toJSON();
 
   expect(tree).toMatchSnapshot();
