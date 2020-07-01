b4c09a7363bf091a757e1045e229145ffc6d16ee
antonmalykhin <a.y.malykhin@gmail.com>
2020-07-01 23:23:47 +0300

    Подключает в игровой процесс проверку правильного ответа

--- src/components/game-screen/game-screen.jsx
+++ src/components/game-screen/game-screen.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {connect} from "react-redux";
 import {GameType} from "../../const.js";
+import Mistakes from "../mistakes/mistakes.jsx";
 
 const GameScreen = (props) => {
-  const {type, children} = props;
+  const {type, children, mistakes} = props;
 
   return (
     <section className={`game game--${type}`}>
@@ -16,11 +18,7 @@ const GameScreen = (props) => {
           <circle className="timer__line" cx="390" cy="390" r="370"
             style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}} />
         </svg>
-        <div className="game__mistakes">
-          <div className="wrong" />
-          <div className="wrong" />
-          <div className="wrong" />
-        </div>
+        <Mistakes count={mistakes} />
       </header>
 
       {children}
@@ -33,7 +31,13 @@ GameScreen.propTypes = {
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node
-  ]).isRequired
+  ]).isRequired,
+  mistakes: PropTypes.number.isRequired,
 };
 
-export default GameScreen;
+const mapStateToProps = (state) => ({
+  mistakes: state.mistakes,
+});
+
+export {GameScreen};
+export default connect(mapStateToProps)(GameScreen);
